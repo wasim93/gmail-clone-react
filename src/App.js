ab9158862b30927a254b8b0cd62bc8ff04ee9d93
Wasim Akram <muhammad.wasim93@gmail.com>
2020-12-30 14:12:25 +0500

    Sidebar Component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Siderbar from './Siderbar';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <h1>Lets Build Gmail!</h1>
+      <Siderbar />
     </div>
   );
 }
