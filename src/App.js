17b3b7cbe1a9d5d7d345eacd0eebe9df6803029c
Wasim Akram <muhammad.wasim93@gmail.com>
2020-12-31 14:39:06 +0500

    React-Router Setup, Basic Routes, Section, EmailList, EmailRow, Email Components & Completed Styling.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Siderbar from './Siderbar';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Mail from './Mail';
+import EmailList from './EmailList';
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Siderbar />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+
+        <div className='app__body'>
+          <Siderbar />
+
+          <Switch>
+            <Route path='/mail' component={Mail} />
+            <Route path='/' component={EmailList} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Siderbar.js
+++ src/Siderbar.js
@@ -1,5 +1,5 @@
-import { Button, IconButton } from '@material-ui/core';
 import React from 'react';
+import { Button, IconButton } from '@material-ui/core';
 import './Sidebar.css';
 import AddIcon from '@material-ui/icons/Add';
 import InboxIcon from '@material-ui/icons/Inbox';
@@ -8,14 +8,11 @@ import StarIcon from '@material-ui/icons/Star';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import LabelImportantIcon from '@material-ui/icons/LabelImportant';
 import NearMeIcon from '@material-ui/icons/NearMe';
-import NoteIcon from '@material-ui/icons/Note';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
 import PersonIcon from '@material-ui/icons/Person';
 import DuoIcon from '@material-ui/icons/Duo';
 import PhoneIcon from '@material-ui/icons/Phone';
-
-import { LabelImportant, NearMe, Note } from '@material-ui/icons';
 
 const Siderbar = () => {
   return (
